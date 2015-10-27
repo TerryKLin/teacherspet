5ffe812850e58842b685018ec46551df74f476ee
Sam <samchurney@gmail.com>
2015-10-27 18:36:05 +0000

    Started ask question form

--- resources/views/forms/question.blade.php
+++ resources/views/forms/question.blade.php
@@ -1,1 +1,9 @@
-<h3>Form here</h3>
+{!! Form::open() !!}
+	<div class="form-group">
+    	{!! Form::textarea('question', null, array('class' => 'form-control', 'placeholder' => 'Why is the sky blue?')) !!}
+    </div>
+
+    <div class="form-group">
+    	{!! Form::button('Ask this question', array('class' => 'btn btn-primary')) !!}
+    </div>
+{!! Form::close() !!}
